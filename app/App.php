948209122c95bcd05ed700eeaa88c3e301d41e83
Edward <edvardtarasov@ya.r>
2021-11-28 22:20:05 +0300

    log

--- app/App.php
+++ app/App.php
@@ -8,4 +8,22 @@ class App
     {
         echo 'Hello app';
     }
+
+    //\+7\(\d{3}\)\s\d{3}-\d{2}-\d{2}
+    //\d{4}(\s|-)\d{4}
+    //\+?\d{1,2}\(\d{3}\)\s?\d{3}-\d{2}-\d{2}
+    public function regexp($data)
+    {
+        $regexp = "/\+?\d{1,2}\(\d{3}\)\s\d{3}-\d{2}-\d{2}/";
+        $result = null;
+        preg_match_all($regexp,$data,$result);
+        foreach ($result[0] as $item){
+            echo $item, "<br>";
+        }
+
+        $regexp = '/\s\s+/';
+        echo  preg_replace($regexp,' ',$data);
+
+    }
+
 }
